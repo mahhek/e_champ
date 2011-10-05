e2b0354a3ece7488c8cbca0f472659e9150b4250
mahhek <mahhek.khan@gmail.com>
2011-10-05 23:18:19 +0500

    fix

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Profile < ActiveRecord::Base
   belongs_to :user
 
@@ -10,16 +9,3 @@ class Profile < ActiveRecord::Base
     :small  => "400x400>" },    
     :path => ":rails_root/public/images/photos/:id/:style/:basename.:extension"
 end
-=======
-class Profile < ActiveRecord::Base
-  belongs_to :user
-
-  validates :hometown, :presence => {:message => 'Please fill in home town!'}
-
-  has_attached_file :photo,
-    :styles => {
-    :thumb=> "100x100#",
-    :small  => "400x400>" },    
-    :path => ":rails_root/public/images/photos/:id/:style/:basename.:extension"
-end
->>>>>>> 81876f719b27be3a9e4332cf54fa11a25d332e45
